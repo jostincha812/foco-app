2863f6f5d6676efbe4f767983bea71160b268fb7
jostincha812 <jostin.cha812@gmail.com>
2016-06-09 22:44:53 -0700

    removed unused buck files

--- FocoApp.js
+++ FocoApp.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux';
 
 import App from './app/app';
-import configureStore from './app/store';
+import configureStore from './app/configStore';
+import configureFirebase from './app/configFirebase';
 
 const store = configureStore();
 
-export default class FocoApp extends Component {
+class FocoApp extends Component {
+  constructor(props) {
+    super(props);
+    configureFirebase();
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -15,3 +21,5 @@ export default class FocoApp extends Component {
     );
   }
 }
+
+export default FocoApp;
