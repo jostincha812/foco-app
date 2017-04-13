774448bb328aff15468399112d4cbeef2b7da89e
jostincha812 <jostin.cha812@gmail.com>
2017-04-13 08:51:28 +0800

    bookmark toggle

--- app/components/UserActivityCard.js
+++ app/components/UserActivityCard.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
 
-import C from '../C';
-import T from '../T';
-import S, { spacing } from '../styles/styles';
-import Card from '../components/Card';
-import TagsList from '../components/TagsList';
-import Icons from '../components/Icons';
+import C from '../C'
+import T from '../T'
+import S, { spacing } from '../styles/styles'
+import Card from '../components/Card'
+import TagsList from '../components/TagsList'
+import BookmarkedToggle from '../components/BookmarkedToggle'
 
 export default class UserActivityCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onToggle = this.onToggle.bind(this)
+  }
+
+  onToggle(bookmarked) {
+    console.log(bookmarked)
+  }
+
   render() {
-    const data = this.props.data;
+    const data = this.props.data
     return (
       <Card containerStyle={styles.activityCard}>
-        <View style={{position:'absolute', top:-spacing.standard+2, right:0}}>
-          {Icons.bookmarkOutline({tintColor: T.inverseColor})}
-        </View>
+        <BookmarkedToggle
+          style={{position:'absolute', top:-spacing.standard+2, right:0}}
+          color={T.inverseColor}
+          bookmarked={data.bookmarked}
+          onToggle={this.onToggle}
+        />
         <Text style={[S.titleBanner, {marginRight:spacing.standard*2}]}>
           {data.title}
         </Text>
         <TagsList max={3} tags={data.tags} theme={C.THEME_ACCENT1} more={C.THEME_ACCENT2} />
       </Card>
-    );
+    )
   }
 }
 
@@ -34,4 +46,4 @@ const styles = StyleSheet.create({
     backgroundColor: T.accentColor2,
     overflow: 'hidden',
   }
-});
+})
