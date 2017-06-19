fdf3655e817d1432ab741bb7732864b05a2c039a
jostincha812 <jostin.cha812@gmail.com>
2017-06-18 22:13:24 -0700

    removed old files

--- app/2.0/actions/userActivitiesActions.js
+++
@@ -1,9 +0,0 @@
-import C from '../C'
-import getData from '../data/api'
-
-export function fetchUserActivities() {
-  return {
-    type: C.FETCH_USER_ACTIVITIES,
-    payload: getData('userActivites')
-  }
-}
--- app/2.0/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import userActivitiesReducer from './userActivitiesReducer'
-
-const rootReducer = combineReducers({
-  activitiesData: userActivitiesReducer
-})
-
-export default rootReducer;
