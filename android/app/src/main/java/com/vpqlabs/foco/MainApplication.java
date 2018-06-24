8108b03c9b4caa9609f5ffa083429a235165d1aa
jostincha812 <jostin.cha812@gmail.com>
2018-06-24 00:26:38 -0700

    running build on AVD

--- android/app/src/main/java/com/vpqlabs/foco/MainApplication.java
+++ android/app/src/main/java/com/vpqlabs/foco/MainApplication.java
@@ -3,33 +3,19 @@ package com.vpqlabs.foco;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.magus.fblogin.FacebookLoginPackage;
 import com.facebook.soloader.SoLoader;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
 import io.invertase.firebase.crash.RNFirebaseCrashPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 
@@ -46,32 +32,18 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage(),
-            new RNI18nPackage(),
-            new RNFirebasePackage(),
-            new FacebookLoginPackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage(),
-            new RNI18nPackage(),
-            new RNFirebasePackage(),
-            new FacebookLoginPackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage(),
-            new RNI18nPackage(),
-            new RNFirebasePackage(),
-            new FacebookLoginPackage(),
-            new RNI18nPackage(),
-            new FacebookLoginPackage(),
           new RNFirebasePackage(),
           new RNFirebaseAnalyticsPackage(),
           new RNFirebaseAuthPackage(),
           new RNFirebaseCrashPackage(),
           new RNFirebaseDatabasePackage(),
+          new RNFirebaseRemoteConfigPackage(),
           new VectorIconsPackage(),
-          new RNSpinkitPackage()
+          new RNSpinkitPackage(),
+          new RNI18nPackage(),
+          new FacebookLoginPackage()
       );
     }
 
